d1d16ad6f5c7974729e74f9bac91e129a5a49972
Soumi De <soumi.de@ligo.org>
2017-04-15 10:33:10 -0400

    Additions similar to jacobian PR first pushed state

--- pycbc/inference/option_utils.py
+++ pycbc/inference/option_utils.py
@@ -18,117 +18,15 @@
 """
 
 import logging
-import numpy
 from pycbc.io import InferenceFile
-from pycbc.workflow import WorkflowConfigParser
 import pycbc.inference.sampler
 from pycbc.inference import likelihood
+from pycbc.inference import sampling_conversions
 from pycbc.pool import choose_pool
 from pycbc.psd import from_cli_multi_ifos as psd_from_cli_multi_ifos
 from pycbc.strain import from_cli_multi_ifos as strain_from_cli_multi_ifos
 from pycbc.gate import gates_from_cli, psd_gates_from_cli, apply_gates_to_td, \
                        apply_gates_to_fd
-
-
-#-----------------------------------------------------------------------------
-#
-#                   Utilities for loading config files
-#
-#-----------------------------------------------------------------------------
-
-def convert_liststring_to_list(lstring):
-    """Checks if an argument of the configuration file is a string of a list
-    and returns the corresponding list (of strings).
-
-    The argument is considered to be a list if it starts with '[' and ends
-    with ']'. List elements should be comma separated. For example, passing
-    `'[foo bar, cat]'` will result in `['foo bar', 'cat']` being returned. If
-    the argument does not start and end with '[' and ']', the argument will
-    just be returned as is.
-    """
-    if lstring[0]=='[' and lstring[-1]==']':
-        lstring = [str(lstring[1:-1].split(',')[n].strip().strip("'"))
-                      for n in range(len(lstring[1:-1].split(',')))]
-    return lstring
-
-
-def add_config_opts_to_parser(parser):
-    """Adds options for the configuration files to the given parser.
-    """
-    parser.add_argument("--config-files", type=str, nargs="+", required=True,
-                        help="A file parsable by "
-                             "pycbc.workflow.WorkflowConfigParser.")
-    parser.add_argument("--config-overrides", type=str, nargs="+",
-                        default=None, metavar="SECTION:OPTION:VALUE",
-                        help="List of section:option:value combinations to "
-                             "add into the configuration file.")
-
-
-def config_parser_from_cli(opts):
-    """Loads a config file from the given options, applying any overrides
-    specified. Specifically, config files are loaded from the `--config-files`
-    options while overrides are loaded from `--config-overrides`.
-    """
-    # read configuration file
-    logging.info("Reading configuration file")
-    if opts.config_overrides is not None:
-        overrides = [override.split(":") for override in opts.config_overrides]
-    else:
-        overrides = None
-    return WorkflowConfigParser(opts.config_files, overrides)
-
-
-def read_args_from_config(cp, section_group=None):
-    """Given an open config file, loads the static and variable arguments to
-    use in the parameter estmation run.
-
-    Parameters
-    ----------
-    cp : WorkflowConfigParser
-        An open config parser to read from.
-    section_group : {None, str}
-        When reading the config file, only read from sections that begin with
-        `{section_group}_`. For example, if `section_group='foo'`, the
-        variable arguments will be retrieved from section
-        `[foo_variable_args]`. If None, no prefix will be appended to section
-        names.
-
-    Returns
-    -------
-    variable_args : list
-        The names of the parameters to vary in the PE run.
-    static_args : dict
-        Dictionary of names -> values giving the parameters to keep fixed.
-    """
-    logging.info("Loading arguments")
-    if section_group is not None:
-        section_prefix = '{}_'.format(section_group)
-    else:
-        section_prefix = ''
-
-    # sanity check that each parameter in [variable_args] has a priors section
-    variable_args = cp.options("{}variable_args".format(section_prefix))
-    subsections = cp.get_subsections("{}prior".format(section_prefix))
-    tags = numpy.concatenate([tag.split("+") for tag in subsections])
-    if not any(param in tags for param in variable_args):
-        raise KeyError("You are missing a priors section in the config file.")
-
-    # get parameters that do not change in sampler
-    static_args = dict([(key,cp.get_opt_tags(
-        "{}static_args".format(section_prefix), key, []))
-        for key in cp.options("{}static_args".format(section_prefix))])
-    # try converting values to float
-    for key,val in static_args.iteritems():
-        try:
-            # the following will raise a ValueError if it cannot be cast to
-            # float (as we would expect for string arguments)
-            static_args[key] = float(val)
-        except ValueError:
-            # try converting to a list of strings; this function will just
-            # return val if it does not begin (end) with [ (])
-            static_args[key] = convert_liststring_to_list(val) 
-
-    return variable_args, static_args
 
 
 #-----------------------------------------------------------------------------
@@ -404,7 +302,7 @@ def results_from_cli(opts, load_samples=True, walkers=None):
     load_samples : {True, bool}
         Load samples from the results file using the parameters, thin_start,
         and thin_interval specified in the options. The samples are returned
-        as a FieldArray instance.
+        as a WaveformArray instance.
     walkers : {None, (list of) int}
         If loading samples, the walkers to load from. If None, will load from
         all walkers.
@@ -417,13 +315,15 @@ def results_from_cli(opts, load_samples=True, walkers=None):
         List of the parameters to use, parsed from the parameters option.
     labels : list
         List of labels to associate with the parameters.
-    samples : {None, FieldArray}
-        If load_samples, the samples as a FieldArray; otherwise, None.
+    samples : {None, WaveformArray}
+        If load_samples, the samples as a WaveformArray; otherwise, None.
     """
+
     logging.info("Reading input file")
     fp = InferenceFile(opts.input_file, "r")
     parameters = fp.variable_args if opts.parameters is None \
                  else opts.parameters
+
     # load the labels
     labels = []
     for ii,p in enumerate(parameters):
@@ -433,6 +333,8 @@ def results_from_cli(opts, load_samples=True, walkers=None):
         else:
             label = fp.read_label(p)
         labels.append(label)
+
+    # load the samples
     if load_samples:
         logging.info("Loading samples")
         samples = fp.read_samples(parameters, walkers=walkers,
@@ -440,6 +342,10 @@ def results_from_cli(opts, load_samples=True, walkers=None):
             thin_end=opts.thin_end, iteration=opts.iteration)
     else:
         samples = None
+
+    # add a set of base parameters if they are not included in InferenceFile
+    samples = sampling_conversions.add_base_parameters(samples)
+
     return fp, parameters, labels, samples
 
 
