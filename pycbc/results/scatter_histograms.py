d80af4e44eb18a8a1cb1a3a0490846eb08491760
Soumi De <soumi.de@ligo.org>
2018-03-04 17:27:57 -0500

    adjust fontsize

--- pycbc/results/scatter_histograms.py
+++ pycbc/results/scatter_histograms.py
@@ -84,7 +84,8 @@ def create_axes_grid(parameters, labels=None, height_ratios=None,
     if ndim < 3:
         fsize = (8, 7)
     else:
-        fsize = (ndim*3 - 1, ndim*3 - 2)
+        #fsize = (ndim*3 - 1, ndim*3 - 2)
+        fsize = (ndim*3 + 1, ndim*3 - 2)
     fig = pyplot.figure(figsize=fsize)
     # create the axis grid
     gs = gridspec.GridSpec(ndim, ndim, width_ratios=width_ratios,
@@ -393,7 +394,7 @@ def create_marginalized_hist(ax, values, label, percentiles=None,
 
 
     if percentiles is None:
-        percentiles = [0., 50., 90.]
+        percentiles = [0., 50., 95.]
     values = numpy.percentile(values, percentiles)
     for val in values:
         if rotated:
