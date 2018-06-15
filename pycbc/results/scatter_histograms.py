9fb9b6e4bc088fe81e55b18ee2532a01d4729171
Soumi De <sde101@syr.edu>
2018-06-15 14:44:25 -0400

    Changes

--- pycbc/results/scatter_histograms.py
+++ pycbc/results/scatter_histograms.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 Miriam Cabero Mueller, Collin Capano
+ (C) 2016 Miriam Cabero Mueller, Collin Capano
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
